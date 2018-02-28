70c8526826d3bb1b49fb5378df51697837e1fdcf
AB <am.id.dqd@gmail.com>
2018-02-28 21:15:44 +0200

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import Calendar from './Calendar';
 import registerServiceWorker from './registerServiceWorker';
 
-let date = new Date();
-date.setFullYear(2010);
-date.setMonth(11);
-
 ReactDOM.render(<Calendar />, document.getElementById('root'));
 
 registerServiceWorker();
